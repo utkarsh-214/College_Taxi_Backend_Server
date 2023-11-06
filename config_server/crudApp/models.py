09025ad11a6beea0b631ae6397cd8fba39ec2fad
kahitoz <kshitiz.sinha.025@gmail.com>
2023-11-06 20:55:10 +0530

    Created ORM for Users table

---
+++ config_server/crudApp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class UserTable(models.Model):
+    UserID = models.AutoField(primary_key=True)
+    UserName = models.CharField(max_length=100)
+    UserEmail = models.CharField(max_length=100)
+    UserNumber = models.CharField(max_length=10)
+
+
